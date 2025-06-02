93279b5c36ea73d8d26026294a57cdaea551e165
Alireza Dehkar <alirezadehkar@gmail.com>
2025-06-02 09:41:38 +0330

    update js build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'cb14342b24a55ddfa807');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'bab4eab29753a2cdfc69');
