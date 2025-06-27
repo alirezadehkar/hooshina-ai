47bed08e0bf8844b20b4d0c0762862d9fc683425
Alireza Dehkar <alirezadehkar@gmail.com>
2025-06-27 19:28:47 +0330

    change style of generator modal

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'f03be52a4ec72fdb66b3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'a11c2cfd97ef1a55a25f');
