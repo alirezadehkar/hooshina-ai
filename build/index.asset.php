4995f2345e7ee26bb27ef4dd3857306bc4b22751
Alireza Dehkar <alirezadehkar@gmail.com>
2025-08-05 17:10:05 +0330

    [fixed] scroll in generator modal

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'a11c2cfd97ef1a55a25f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'c86596429186c73799e8');
